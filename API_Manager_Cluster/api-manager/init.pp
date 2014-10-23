b11b8c591bb3506d8b1c1d984b5e99eada91a2e7
Yasassri Ratnayake <ycrnet@gmail>
2014-10-23 15:29:30 +0530

    API-M

--- API_Manager_Cluster/api-manager/init.pp
+++ API_Manager_Cluster/api-manager/init.pp
@@ -5,7 +5,7 @@ class params {
 # General Settings
 
 # File Locations
-  $deployment_target  = '/home/yasassri/Desktop/QA_Resources/puppet/DEPLOY'
+  $deployment_target  = '/home/yasassri/Desktop/QA_Resources/puppet/DEPLOY/APMTEST'
   $pack_location      = '/home/yasassri/Desktop/soft/WSO2_Products/API_Manager/new'
   $script_base_dir  = inline_template("<%= Dir.pwd %>") #location will be automatically picked up
 
@@ -49,57 +49,90 @@ class params {
   $apim_db_username		= 'apimuser2'
   $apim_db_password   = 'wso2root'
 
-##################LDAP Configuration###################
 
-  $connect_to_ldap  = true # use an external Ldap as the primary user store. The Default JDBC userstore wil not b connected in this case.
+# WSO2_Carbon DBs, local DBs for each node
+
+  $gw_manager_carbondb_names  = ['gwmgrcarbondb']
+  $gw_manager_carbondb_usernames  = ['apimuser2']
+  $gw_manager_carbondb_passwords  = ['wso2root']
+
+  $gw_worker_carbondb_names  = ['gwwrk1carbondb','gwwrk2carbondb']
+  $gw_worker_carbondb_usernames  = ['apimuser2','apimuser2']
+  $gw_worker_carbondb_passwords  = ['wso2root','wso2root']
+
+  $km_carbondb_names  = ['km1carbondb','km2carbondb']
+  $km_carbondb_usernames  = ['apimuser2','apimuser2']
+  $km_carbondb_passwords  = ['wso2root','wso2root']
+
+  $publisher_carbondb_names  = ['storecarbondb']
+  $publisher_carbondb_usernames  = ['apimuser2']
+  $publisher_carbondb_passwords  = ['wso2root']
+
+  $store_carbondb_names  = ['pubcarbondb']
+  $store_carbondb_usernames  = ['apimuser2']
+  $store_carbondb_passwords  = ['wso2root']
+
+
+##################LDAP Configuration###################
+  $connect_to_ldap  = false # use an external Ldap as the primary user store. The Default JDBC userstore wil not be disconnected in this case.
   $ldap_url         = '192.168.18.76'
   $ldap_port        = '389'
   # Ldap Connection name properties
-  $ldap_cn    = 'wso2'
-  $ldap_dc    = ''
+
   $connection_name = 'cn=admin,dc=ITIndustry,dc=sl' # This goes into the user manager.xml
   $rootpartition_string = 'dc=ITIndustry,dc=sl' # This goes into tenent-management.xml
 
+ ################# Dep Sync Configs ###############
+
+  $dep_sync_enabled = true
+  $svn_url = 'http://svnexample.wso2.com/svn/yasassri/apimpuppet'
+  $svn_user_name  = 'wso2'
+  $svn_password   = 'wso2123'
+
+  ############### remote Deploying and server startup###############3
+
+  $copy_nodes_to_remote_machines  = false
+  $start_up_remote_servers        = false
+  $remote_target_directory        = '/home/wso2qa/api-m_puppet'
+  $user_name                      = "wso2qa" # This is the OS level username that is required to ssh
 
 ##################################
 #### KM Related Configs ##########
 
-  $km_clustering = true
-
   # Manager Nodes Parameters only configure following if clustering true for the KM
-  $km_manager_offsets             = ['1']
-  $km_manager_hosts               = ['apim.180.km.com']
-  $km_manager_ips                 = ['10.100.5.112']
-  $km_manager_local_member_ports  = ['4001']
+  $km_manager_offsets             = ['1','6']
+  $km_manager_hosts               = ['apim.180.release.km.com','apim.180.release.km.com']
+  $km_manager_ips                 = ['10.100.5.112','10.100.5.112']
+  $km_manager_local_member_ports  = ['4001','4007']
 
   # Worker Nodes parameters
   $km_worker_offsets            = ['6']
-  $km_worker_hosts              = ["apim.180.km.worker.com"] # Number of Nodes are determined from the array length
+  $km_worker_hosts              = [] # Number of Nodes are determined from the array length
   $km_worker_ips                = ['10.100.5.112']
   $km_worker_local_member_ports = ['4007']
 
 ######################################
 ##### GateWay Related Configs ########
 
-  $gw_clustering = true
-
-# Manager Nodes Parameters,,,, only configure following if clustering true for the Gate Way
+ # Manager Nodes Parameters,,,, only configure following if clustering true for the Gate Way
   $gw_manager_offsets            = ['2']
-  $gw_manager_hosts              = ["apim.180.gw.com"] # Number of Nodes are determined from the array length
+  $gw_manager_hosts              = ["apim.180.release.gw.manager.com"] # Number of Nodes are determined from the array length
   $gw_manager_ips                = ['10.100.5.112']
   $gw_manager_local_member_ports = ['4003']
 
+  #$gw_local_db_names             = []
+
 # Worker Nodes parameters
-  $gw_worker_offsets = ['5']
-  $gw_worker_hosts = ["apim.180.gw.worker.com"] # Number of Nodes are determined from the array length
-  $gw_worker_ips = ['10.100.5.112']
-  $gw_worker_local_member_ports = ['4006']
+  $gw_worker_offsets = ['5','7']
+  $gw_worker_hosts = ["apim.180.release.gw.worker.com","apim.180.release.gw.worker.com"] # Number of Nodes are determined from the array length
+  $gw_worker_ips = ['10.100.5.112','10.100.5.112']
+  $gw_worker_local_member_ports = ['4006','4008']
 
 ################################################
 ############# Publisher Related Configs ########
 
   $publisher_offsets            = ['3']
-  $publisher_hosts              = ['apim.180.publisher.com']
+  $publisher_hosts              = ['apim.180.release.publisher.com']
   $publisher_ips                = ['10.100.5.112']
   $publisher_local_member_ports = ['4004']
 
@@ -107,14 +140,14 @@ class params {
 ########### Store Related Configs ###########
 
   $store_offsets            = ['4']
-  $store_hosts              = ['apim.180.store.com']
+  $store_hosts              = ['apim.180.release.store.com']
   $store_ips                = ['10.100.5.112']
   $store_local_member_ports = ['4005']
 
 #######cluster details#########
-  $km_domain_name = "apim.qa.km.180"
-  $gw_domain_name = "apim.qa.gw.180"
-  $pub_store_domain = "apim.qa.storepub.180"
+  $km_domain_name = "apim.release.km.180"
+  $gw_domain_name = "apim.release.gw.180"
+  $pub_store_domain = "apim.release.storepub.180"
 
 ####### ELB Related Configs ########### api-manager.xml
 
@@ -123,8 +156,8 @@ class params {
   $elb_gw_group_mgt_port = "4060"
   $elb_store_pub_group_mgt_port = "4070"
 
-  $km_cluster_domain = "apim.180.km.com" # Give the common host name of KM nodes api-manager.xml
-  $gw_cluster_domain = "apim.180.gw.com" # Give the common host name of gw nodes api-manager.xml
+  $km_cluster_domain = "apim.180.release.km.com" # Give the common host name of KM nodes api-manager.xml
+  $gw_cluster_domain = "apim.180.release.gw.worker.com" # Give the common host name of gw nodes api-manager.xml
   $cluster_port_https = "443" # elb listner Ports
   $cluster_port_http = "80"
 
@@ -172,6 +205,7 @@ class publisher_deploy inherits params {
 
  loop{"301":
     count=>301,
+
     setupnode => "publisher",
     deduct => 300
   }
@@ -194,7 +228,6 @@ class gw_deploy inherits params {
         count=>$gw_manager_nodes+100,
         setupnode => "gw-manager",
         deduct => 100
-
       }
 
     #Configuring worker Nodes
@@ -241,6 +274,14 @@ class create_loadblnc_conf_configs inherits params {
     content => template("${params::script_base_dir}/templates/elb/load_balancer_configs.xml.erb"),
     require => File["${params::deployment_target}/elb-configs"],
   }
+
+  file {"${params::deployment_target}/elb-configs/axis2.xml":
+
+    ensure => present,
+    mode    => '0755',
+    content => template("${params::script_base_dir}/templates/elb/axis2.xml.erb"),
+    require => File["${params::deployment_target}/elb-configs"],
+  }
 }
 
 # Loop for Spawning Members
@@ -261,7 +302,7 @@ define loop($count,$setupnode,$deduct) {
   #copying the Files (packs)
     exec { "Copying_$setupnode-$number":
 
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin', # The search path used for command execution.
+      path    => '/usr/local/sbin:/usr/localkm_manager_ips/bin:/usr/sbin:/usr/bin:/sbin:/bin', # The search path used for command execution.
       command => "cp -r ${params::pack_location}/wso2am-*/* ${params::deployment_target}/$setupnode-$number/",
       require => File["${params::deployment_target}/$setupnode-$number"],
     }
@@ -298,6 +339,7 @@ define loop($count,$setupnode,$deduct) {
       }
 }
 
+    # To handle Key Manager related stuff
     if($setupnode == "km-manager" or $setupnode == "km-worker"){
 
       $local_names3 = regsubst($params::km_deployment_configs, '$', "-$name")
@@ -306,6 +348,11 @@ define loop($count,$setupnode,$deduct) {
         node_number => $number,
         nodes => $setupnode
       }
+   }
+
+    remote_copy{"remote_cpy_$setupnode-$name":
+    node_name=>$setupnode,
+    node_number=>$number
     }
 
   $next = $name + 1
@@ -327,7 +374,6 @@ define copy_files($from,$to,$node_name,$unq_id){
       Package['rsync'],
       Exec["Copying_$node_name"]
     ],
-
   }
 }
 
@@ -341,6 +387,45 @@ define pushTemplates($node_number,$nodes) {
     mode    => '0755',
     content => template("${params::script_base_dir}/templates/${nodes}/${orig_name}.erb"),
     require => Exec["Copying_${nodes}-${node_number}"],
+  }
+}
+
+define remote_copy($node_name,$node_number){
+
+  if($params::copy_nodes_to_remote_machines==true){
+
+    case $node_name {
+      gw-manager: {$remote_node_ip = $params::gw_manager_ips[$node_number-1]}
+      gw-worker : {$remote_node_ip = $params::gw_worker_ips[$node_number-1]}
+      km-manager: {$remote_node_ip = $params::km_manager_ips[$node_number-1]}
+      km-worker : {$remote_node_ip = $params::km_worker_ips[$node_number-1]}
+      publisher : {$remote_node_ip = $params::publisher_ips[$node_number-1]}
+      store     : {$remote_node_ip = $params::store_ips[$node_number-1]}
+    }
+    remote_copy_start_servers{"Remote_Copy_${node_name}-${node_number}":
+      remote_ip=> $remote_node_ip,
+      user_name=>"${params::user_name}",
+      node_name=>"${node_name}",
+      node_number=> "${node_number}"
+    }
+  }
+}
+
+define remote_copy_start_servers($remote_ip,$user_name,$node_name,$node_number){
+
+  exec { "Cpy_to_remote_$node_name-$node_number":
+    path    => '/usr/bin:/bin',
+    command => "rsync -r ${params::deployment_target}/${node_name}-${node_number} ${user_name}@${remote_ip}:${params::remote_target_directory}",
+    require =>  File ["$params::deployment_target/$node_name-${node_number}/repository/conf/api-manager.xml","$params::deployment_target/$node_name-${node_number}/repository/conf/datasources/master-datasources.xml"]
+  }
+
+  if($params::start_up_remote_servers==true){
+
+    exec { "Start_servers_$node_name-$node_number":
+      path    => '/usr/bin:/bin',
+      command => "ssh ${user_name}@${remote_ip} '${params::remote_target_directory}/${node_name}-$node_number/bin/wso2server.sh start'",
+      require => Exec["Cpy_to_remote_$node_name-$node_number"]
+    }
   }
 }
 
